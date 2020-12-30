dfd44ef32b8bf988315b5cc2a00e8ac22036ebbd
Dylan Gore <hello@dylangore.ie>
2020-12-30 23:05:48 +0000

    Add basic navigation

--- app/src/main/java/ie/dylangore/mad1/assignment2/MainActivity.kt
+++ app/src/main/java/ie/dylangore/mad1/assignment2/MainActivity.kt
@@ -1,17 +1,31 @@
 package ie.dylangore.mad1.assignment2
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import ie.dylangore.mad1.assignment2.ui.fragments.LocationsFragment
+import ie.dylangore.mad1.assignment2.ui.fragments.ObservationStationsFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_locations, R.id.nav_observation_stations))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
     }
+
 }
